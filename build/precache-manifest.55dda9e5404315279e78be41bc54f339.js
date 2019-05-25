794dfee98043ae57d6af944934d99149b663a4b4
Dennis <dennissarmiento808@gmail.com>
2019-05-25 15:39:44 -0700

    up

---
+++ build/precache-manifest.55dda9e5404315279e78be41bc54f339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6316c69c1582ebb6c82e",
+    "url": "/wineapp2/static/css/main.ea002138.chunk.css"
+  },
+  {
+    "revision": "6316c69c1582ebb6c82e",
+    "url": "/wineapp2/static/js/main.7c31710f.chunk.js"
+  },
+  {
+    "revision": "30788f26fe46c3c148bb",
+    "url": "/wineapp2/static/js/runtime~main.f0029ef6.js"
+  },
+  {
+    "revision": "3861845479a4748ef1bb",
+    "url": "/wineapp2/static/js/2.fb396b31.chunk.js"
+  },
+  {
+    "revision": "a9a195b37e4ff7562236afa9d016f7c8",
+    "url": "/wineapp2/index.html"
+  }
+];
